f30bfd881fdc0dac3f2b71adea5a44b12b074d81
W-L <weilguny.lukas@gmail.com>
2026-03-04 15:44:36 +0100

    update manager import and remove lines killing processes

--- code/stop_minknow_run.py
+++ code/stop_minknow_run.py
@@ -1,24 +1,22 @@
-import minknow_api
+from minknow_api.manager import Manager
 import subprocess
 import shlex
 
-try:
-    m = minknow_api.manager.Manager()
-    p = list(m.flow_cell_positions())[0]
-    c = p.connect()
 
-    c.protocol.stop_protocol()
-except:
-    pass
+m = Manager()
+p = list(m.flow_cell_positions())[0]
+c = p.connect()
+
+c.protocol.stop_protocol()
 
 
-dor = subprocess.run(shlex.split("pkill dorado"), capture_output=True, text=True)
-print(dor.stdout)
-print(dor.stderr)
-mkm = subprocess.run(shlex.split("pkill mk_manager"), capture_output=True, text=True)
-print(mkm.stdout)
-print(mkm.stderr)
-mkw = subprocess.run(shlex.split("pkill minknow"), capture_output=True, text=True)
-print(mkw.stdout)
-print(mkw.stderr)
+#dor = subprocess.run(shlex.split("pkill dorado"), capture_output=True, text=True)
+#print(dor.stdout)
+#print(dor.stderr)
+#mkm = subprocess.run(shlex.split("pkill mk_manager"), capture_output=True, text=True)
+#print(mkm.stdout)
+#print(mkm.stderr)
+#mkw = subprocess.run(shlex.split("pkill minknow"), capture_output=True, text=True)
+#print(mkw.stdout)
+#print(mkw.stderr)
 
